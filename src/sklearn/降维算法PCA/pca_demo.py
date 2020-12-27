4e591f48115b9ef630b8bfab7bb9f14d908a0cc8
yqq <1037123792@qq.com>
2020-12-27 17:01:42 +0800

    pca code

--- src/sklearn/降维算法PCA/pca_demo.py
+++ src/sklearn/降维算法PCA/pca_demo.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import pandas as pd
+import numpy as np
 
 # 准备数据集
 iris = load_iris()
@@ -29,6 +30,7 @@ X_dr = pca.fit_transform(X_DF)  # 训练模型
 # bool索引
 # print(X_dr[y == 0, 0].shape)
 
+"""
 
 colors = ['red','black','orange']
 plt.Figure()
@@ -43,8 +45,23 @@ for i in [0, 1, 2]:
 plt.legend()
 plt.title('PCA of IRIS data sets')
 plt.show()
+"""
 
+print(pca.explained_variance_)
 
+print(pca.explained_variance_ratio_)
 
+# pca_line = PCA().fit(X)
+#
+# plt.plot([1, 2, 3, 4], np.cumsum(pca_line.explained_variance_ratio_))
+# plt.xticks([1, 2, 3, 4])  # 这是为了限制坐标轴显示为整数
+# plt.xlabel("number of components after dimension reduction")
+# plt.ylabel("cumulative explained variance ratio")
+# plt.show()
 
+# 最大似然估计自选超参数
+
+pca_mle = PCA(n_components='mle')
+x_mle = pca_mle.fit_transform(X)
+print(x_mle.shape)
 
