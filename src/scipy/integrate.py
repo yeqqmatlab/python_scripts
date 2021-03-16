e6393b66b75ad3cd409bd7b2494ee6145bda6280
yqq <1037123792@qq.com>
2021-03-16 13:13:31 +0800

    add cpu_scriptd

--- src/scipy/integrate.py
+++ src/scipy/integrate.py
@@ -4,7 +4,6 @@ from scipy import integrate
 import numpy as np
 
 Y = lambda x: x ** 2 + 3
-
 x = np.linspace(-2, 4, 10)
 y = Y(x)
 print(x)
---
+++ src/scripts/monitor_cpu_mem.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+"""
+this is a system monitor scripts
+author yqq
+"""
+
+import os
+import time
+import sys
+import datetime
+import socket
+import psutil
+import re
+import json
+import commands
+
+
+
