f5e04212e69e7b8f692161ddceaeb720a5273825
yqq <1037123792@qq.com>
2021-03-17 13:55:56 +0800

    add monitor cpu mem

---
+++ src/scripts/kafka.py
@@ -0,0 +1,9 @@
+
+
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='192.168.0.121:9092')  # 连接kafka
+
+msg = "Hello World".encode('utf-8')  # 发送内容,必须是bytes类型
+producer.send('test', msg)  # 发送的topic为test
+producer.close()
--- src/scripts/monitor_cpu_mem.py
+++ src/scripts/monitor_cpu_mem.py
@@ -5,15 +5,24 @@ this is a system monitor scripts
 author yqq
 """
 
-import os
 import time
-import sys
-import datetime
-import socket
 import psutil
-import re
-import json
-import commands
+import datetime
 
+now_time = datetime.datetime.now().strftime('%Y_%m_%d')
 
+# print(now_time)
+with open("monitor_cpu_mem_"+now_time+".csv", "a+") as f:
+    # f.write("time,cpu%,mem%\n")  # titles
+
+    current_time = time.strftime('%Y%m%d-%H%M%S', time.localtime(time.time()))
+    # 获取物理内存使用
+    mem = psutil.virtual_memory()
+    # 获取cpu使用率
+    cpu_percent = psutil.cpu_percent(1)
+    line = current_time + ',' + str(cpu_percent) + ',' + str(mem[2])
+    # print(line)
+    f.write(line + "\n")
+    # 每5秒执行一次
+    # time.sleep(2)
 
