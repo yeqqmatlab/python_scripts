90d89ede9331b843bac3c34ae2679fbe66e65aa6
yqq <1037123792@qq.com>
2021-07-23 17:58:50 +0800

    tf code update

--- src/tensorflow/chpter01/tf_demo.py
+++ src/tensorflow/chpter01/tf_demo.py
@@ -1,7 +1,13 @@
 import tensorflow as tf
 
 a = tf.constant([[1, 4], [5, 6], [5, 9]], dtype=tf.int64)
+"""
+demo
+hello
 
+"""
 print("a:", a)
 print("a.dtype:", a.dtype)
 print("a.shape", a.shape)
+
+print(help(tf.constant))
